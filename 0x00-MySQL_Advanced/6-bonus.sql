8877be556aa77571654d3b884f639b630e890b7e
nasergamal <naser.gamal98@gmail.com>
2023-12-14 19:12:13 +0200

    sql mandatory

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,16 @@
+-- stored procedure AddBonus that adds a new correction for a student.
+DELIMITER $$
+CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
+BEGIN
+  IF ( SELECT COUNT(*) FROM projects WHERE name=project_name ) = 0 
+  THEN
+    INSERT INTO projects (name) VALUES ( project_name) ;
+  END IF;
+  INSERT INTO corrections (user_id, project_id, score)
+  VALUES (
+	user_id,
+	( SELECT id FROM projects WHERE name=project_name LIMIT 1),
+	score
+  );
+END$$
+DELIMITER ;
---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,9 @@
+-- stored procedure AddBonus that adds a new correction for a student.
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+  DECLARE average FLOAT;
+  SET average = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id);
+  UPDATE users SET average_score = average WHERE id = user_id;
+END$$
+DELIMITER ;
