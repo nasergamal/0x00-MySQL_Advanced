44acfa4cfad620f685ce8bdcbde306e7e7d61363
nasergamal <naser.gamal98@gmail.com>
2023-12-19 01:22:06 +0200

    102- typo and missing output

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -4,12 +4,14 @@ from pymongo import MongoClient
 
 if __name__ == "__main__":
     logs = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
-    print(logs.count_documents({}), ' logs')
+    print(logs.count_documents({}), 'logs')
     print('Methods:')
     method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for m in method:
         count = logs.count_documents({"method": m})
         print(f'\tmethod {m}: {count}')
+    print(logs.count_documents({"method": "GET", "path": "/status"}),
+          'status check')
     print("IPs:")
     top = logs.aggregate([
         {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
