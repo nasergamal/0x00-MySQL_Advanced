b78c8396d868aa0889e2ccfce6bc222bf9b5f645
nasergamal <naser.gamal98@gmail.com>
2023-12-20 20:26:55 +0200

    documentation and annotation

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,29 +1,30 @@
 #!/usr/bin/env python3
 """
-    page content
+    page content with cache and count
 """
 from functools import wraps
 import redis
 import requests
+from typing import Callable
 r = redis.Redis()
 
 
-def counter(method):
+def url_counter(method: Callable) -> Callable:
     '''count link access'''
     @wraps(method)
-    def wrapper(url):
-        '''wrapper'''
+    def wrapper(url: str) -> str:
+        '''wrapper for caching site content and counting access'''
         html = r.get(f'cache:{url}')
+        r.incr(f'count:{url}')
         if html:
             return html.decode('utf-8')
         html = method(url)
-        r.incr(f'count:{url}')
-        r.set(f'cache:{url}', html, ex=10)
+        r.setex(f'cache:{url}', 10, html)
         return html
     return wrapper
 
 
-@counter
+@url_counter
 def get_page(url: str) -> str:
     '''
     return html content and count page visit times
